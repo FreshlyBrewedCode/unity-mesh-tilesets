3fcda314ecb5e93ebce6a0961328f22ccfc3b508
FreshlyBrewedCode <11772241+FreshlyBrewedCode@users.noreply.github.com>
2021-01-23 14:03:11 +0100

    fix: adjust reflection code in `SceneOverlayWindow` for Unity 2019
    fix #2

--- Editor/SceneOverlayWindow.cs
+++ Editor/SceneOverlayWindow.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Reflection;
 using UnityEditor;
 using UnityEngine;
@@ -7,36 +8,64 @@ namespace MeshTilesetsEditor
 {
     public class SceneOverlayWindow
     {
+        public delegate void WindowFunction(SceneView sceneView);
+
         private object sceneOverlayWindow;
-        private MethodInfo showWindow;
+        private MethodInfo windowMethod;
+        private object[] windowMethodParams;
         public UnityEngine.Object target;
     
         // public delegate void ShowWindowFunc(object overlayWindow);
         public delegate void OnWindowGUICallback(UnityEngine.Object target, SceneView sceneView);
-    
+        
         public SceneOverlayWindow(GUIContent title, OnWindowGUICallback onWindowGUI, UnityEngine.Object target, int priority = int.MaxValue)
         {
             this.target = target;
             var unityEditor = Assembly.GetAssembly(typeof(UnityEditor.SceneView));
+            
+#if UNITY_2019_3 || UNITY_2019_4
+            var overlayWindowType = unityEditor.GetType("UnityEditor.SceneViewOverlay+OverlayWindow");
+#elif UNITY_2020_1_OR_NEWER
             var overlayWindowType = unityEditor.GetType("UnityEditor.OverlayWindow");
+#endif
             var sceneViewOverlayType = unityEditor.GetType("UnityEditor.SceneViewOverlay");
             var windowFuncType = sceneViewOverlayType.GetNestedType("WindowFunction");
-            var windowFunc = Delegate.CreateDelegate(windowFuncType, onWindowGUI.Target, onWindowGUI.Method);
+            var sceneViewFuncDelegate = Delegate.CreateDelegate(windowFuncType, onWindowGUI.Target, onWindowGUI.Method);
+
             var windowDisplayOptionType = sceneViewOverlayType.GetNestedType("WindowDisplayOption");
+            var windowDisplayOption = Enum.Parse(windowDisplayOptionType, "OneWindowPerTarget");
+            
+#if UNITY_2019_3 || UNITY_2019_4
+                windowMethod = sceneViewOverlayType.GetMethods(System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.Public).FirstOrDefault(t => t.Name == "Window" && t.GetParameters().Length == 6);
+#elif UNITY_2020_1_OR_NEWER  
+                //public static void ShowWindow(OverlayWindow window)
+                windowMethod = sceneViewOverlayType.GetMethod("ShowWindow", System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.Public);
+#endif
+
+#if UNITY_2019_3 || UNITY_2019_4
+            windowMethodParams = new object[]
+            { 
+                title, sceneViewFuncDelegate, priority, target, windowDisplayOption, null
+            };
+#elif UNITY_2020_1_OR_NEWER
+            //public OverlayWindow(GUIContent title, SceneViewOverlay.WindowFunction guiFunction, int primaryOrder, Object target, SceneViewOverlay.WindowDisplayOption option)
             sceneOverlayWindow = Activator.CreateInstance(overlayWindowType, 
                 title,
                 windowFunc,
                 int.MaxValue, this.target, 
-                Enum.Parse(windowDisplayOptionType, "OneWindowPerTarget") //SceneViewOverlay.WindowDisplayOption.OneWindowPerTarget
+                windowDisplayOption //SceneViewOverlay.WindowDisplayOption.OneWindowPerTarget
             );
-            showWindow = sceneViewOverlayType.GetMethod("ShowWindow", BindingFlags.Static | BindingFlags.Public);
-
+            windowMethodParams = new object[] { overlayWindow };
+#endif
+            
+            //showWindow = sceneViewOverlayType.GetMethod("ShowWindow", BindingFlags.Static | BindingFlags.Public);
             // showWindow = Delegate.CreateDelegate(typeof(ShowWindowFunc), showSceneViewOverlay) as ShowWindowFunc;
         }
 
         public void ShowWindow()
         {
-            this.showWindow.Invoke(null, new object[]{sceneOverlayWindow});
+            if (windowMethod != null)
+                windowMethod.Invoke(null, windowMethodParams);
         }
 
     }
